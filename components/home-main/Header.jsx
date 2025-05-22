69397ace8f5df8c1a58e3be1bcea9884c27fda1a
ilhemmissaoui <ilhem.missaoui22@gmail.com>
2025-05-22 17:24:10 +0100

    make the videos hosted

--- components/home-main/Header.jsx
+++ components/home-main/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div>
       <video
-        src="/assets/videos/Final Comp_5.mp4"
+        src="https://www.dropbox.com/scl/fi/5ho9ph3qgknhbumrm4r1f/Final-Comp_5-1.mp4?rlkey=8a3fp07moq2ujbg8r8agmn3va&st=3ep5agc7&raw=1"
         autoPlay
         loop
         muted
--- components/page-tiktok-details/Header.jsx
+++ components/page-tiktok-details/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div>
       <video
-        src="/assets/videos/Final Comp.mp4"
+        src="https://www.dropbox.com/scl/fi/wdk6cy3c9sl6i8rho3hn5/Final-Comp.mp4?rlkey=atjxt60murhv72v34unmhoowr&st=uxzrje15&raw=1"
         autoPlay
         loop
         muted
